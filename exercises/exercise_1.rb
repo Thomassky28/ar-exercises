250e279b1b7941dc11c0b8245388b6c5739ea704
Thomassky28 <haifeng3@ualberta.ca>
2018-09-12 18:18:31 +0000

    creat 3 stores, and update and delete stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,43 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
+
+store1 = Store.new do |u|
+  u.name = "Burnaby"
+  u.annual_revenue = 300000
+  u.mens_apparel = true
+  u.womens_apparel = true
+end
+store1.save!
+
+store2 = Store.new do |u|
+  u.name = "Richmond"
+  u.annual_revenue = 1260000
+  u.mens_apparel = false
+  u.womens_apparel = true
+end
+store2.save!
+
+store3 = Store.new do |u|
+  u.name = "Gastown"
+  u.annual_revenue = 190000
+  u.mens_apparel = true
+  u.womens_apparel = false
+end
+store3.save!
+
+pp Store.count
+
+
+# pp Store.all
+# stores =
+
+@store1 = Store.find_by(id: 1);
+puts @store1.name
+# @store1 -> an instance of Store
+# nil
+
+
+# puts Store.sum()
+
 # Your code goes below here ...
